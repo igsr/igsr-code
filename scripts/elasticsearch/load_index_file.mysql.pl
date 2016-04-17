2d9952d9caf3de96007b284279e8766a8db4ea6c
streeter <streeter@ebi.ac.uk>
2016-04-17 17:17:13 +0100

    scripts to populate the file tables of the mysql website database

---
+++ scripts/elasticsearch/load_index_file.mysql.pl
@@ -0,0 +1,130 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use DBI;
+
+my $index_file = '/nfs/1000g-archive/vol1/ftp/data_collections/1000_genomes_project/1000genomes.sequence.index';
+my $data_collection = '1000genomes';
+my $data_type = 'sequence';
+my $analysis_group;
+my ($use_column_headers, $infer_sample);
+my (@url_column, @md5_column);
+my ($analysis_group_column, $withdrawn_column, $sample_column);
+my ($dbname, $dbhost, $dbuser, $dbport, $dbpass) = ('igsr_website', 'mysql-g1kdcc-public', 'g1krw', 4197, undef);
+my $root = '';
+
+&GetOptions(
+  'file|index_file=s'      => \$index_file,
+  'data_collection=s' => \$data_collection,
+  'data_type=s' => \$data_type,
+  'root=s'        => \$root,
+  'dbpass=s'      => \$dbpass,
+  'dbport=i'      => \$dbport,
+  'dbuser=s'      => \$dbuser,
+  'dbhost=s'      => \$dbhost,
+  'dbname=s'      => \$dbname,
+
+  'url_column=s' => \@url_column,
+  'md5_column=s' => \@md5_column,
+  'withdrawn_column=s' => \$withdrawn_column,
+  'sample_column=s' => \$sample_column,
+  'analysis_group_column=s' => \$analysis_group_column,
+
+  'use_column_headers' => \$use_column_headers,
+  'infer_sample' => \$infer_sample,
+);
+die "did not get url column on command line" if ! scalar @url_column;
+die "need one md5 column for each url column" if @md5_column && (scalar @url_column != scalar @md5_column);
+
+my $dbh = DBI->connect("DBI:mysql:$dbname;host=$dbhost;port=$dbport", $dbuser, $dbpass) or die $DBI::errstr;
+my $insert_file_sql = 'INSERT INTO file(url, url_crc, md5) VALUES(?, crc32(?), ?) ON DUPLICATE KEY UPDATE file_id=LAST_INSERT_ID(file_id)';
+my $update_type_sql = 'UPDATE file SET data_type_id=(SELECT data_type_id FROM data_type WHERE code=?) WHERE file_id=?';
+my $update_analysis_group_sql = 'UPDATE file SET analysis_group_id=(SELECT analysis_group_id FROM analysis_group WHERE code=?) WHERE file_id=?';
+my $insert_data_collection_sql = 'INSERT IGNORE INTO file_data_collection(file_id, data_collection_id) SELECT ?, data_collection_id from data_collection where code=?';
+my $insert_sample_sql = 'INSERT IGNORE INTO sample_file(file_id, sample_id) SELECT ?, sample_id from sample where name=?';
+my $sth_file = $dbh->prepare($insert_file_sql) or die $dbh->errstr;
+my $sth_type = $dbh->prepare($update_type_sql) or die $dbh->errstr;
+my $sth_analysis_group = $dbh->prepare($update_analysis_group_sql) or die $dbh->errstr;
+my $sth_data_collection = $dbh->prepare($insert_data_collection_sql) or die $dbh->errstr;
+my $sth_sample = $dbh->prepare($insert_sample_sql) or die $dbh->errstr;
+
+open my $fh, '<', $index_file or die "could not open index_file $!";
+
+my (@i_url, @i_md5, $i_analysis_group, $i_withdrawn, $i_sample);
+
+if ($use_column_headers) {
+  LINE:
+  while (my $line = <$fh>) {
+    next LINE if $line =~ /^##/;
+    chomp $line;
+    $line =~ s/^#//;
+    my @split_line = split("\t", $line);
+    my %cols;
+    foreach my $i (0..$#split_line) {
+      $cols{uc($split_line[$i])} = $i;
+    }
+    @i_url = map {$cols{$_}} @url_column;
+    @i_md5 = map {$cols{$_}} @md5_column;
+    $i_analysis_group = $analysis_group_column ? $cols{$analysis_group_column} : undef;
+    $i_withdrawn = $withdrawn_column ? $cols{$withdrawn_column} : undef;
+    $i_sample = $sample_column ? $cols{$sample_column} : undef;
+    last LINE;
+  }
+}
+else {
+  @i_url = @url_column;
+  @i_md5 = @md5_column;
+  ($i_analysis_group, $i_withdrawn, $i_sample) = ($analysis_group_column, $withdrawn_column, $i_sample);
+}
+
+die "do not know column of url" if grep {!defined $_} @i_url;
+die "do not know column of md5" if grep {!defined $_} @i_md5;
+
+LINE:
+while (my $line = <$fh>) {
+  chomp $line;
+  my @split_line = split("\t", $line);
+  next LINE if defined $i_withdrawn && $split_line[$i_withdrawn];
+  foreach my $j (0..$#i_url) {
+    my ($i_url, $i_md5) = ($i_url[$j], $i_md5[$j]);
+    my $url = $root.$split_line[$i_url];
+    $sth_file->bind_param(1, $url);
+    $sth_file->bind_param(2, $url);
+    $sth_file->bind_param(3, defined $i_md5 ? $split_line[$i_md5] : undef);
+    $sth_file->execute() or die $sth_file->errstr;
+    my $file_id = $sth_file->{mysql_insertid};
+
+    if (my $file_analysis_group = $analysis_group ? $analysis_group
+                        : defined($i_analysis_group) ? lc($split_line[$i_analysis_group]) : undef) {
+      $file_analysis_group =~ s/ /_/g;
+      $sth_analysis_group->bind_param(1, $file_analysis_group);
+      $sth_analysis_group->bind_param(2, $file_id);
+      $sth_analysis_group->execute() or die $sth_analysis_group->errstr;
+    }
+
+    if ($data_type) {
+      $sth_type->bind_param(1, $data_type);
+      $sth_type->bind_param(2, $file_id);
+      $sth_type->execute() or die $sth_type->errstr;
+    }
+
+    if ($data_collection) {
+      $sth_data_collection->bind_param(1, $file_id);
+      $sth_data_collection->bind_param(2, $data_collection);
+      $sth_data_collection->execute() or die $sth_data_collection->errstr;
+    }
+
+    my $sample = defined $i_sample ? $split_line[$i_sample] : undef;
+    if (!$sample && $infer_sample) {
+      ($sample) = $url =~ m{/data/(?:[A-Z]{3}/)?([^\./]+)/};
+    }
+    if ($sample) {
+      $sth_sample->bind_param(1, $file_id);
+      $sth_sample->bind_param(2, $sample);
+      $sth_sample->execute() or die $sth_sample->errstr;
+    }
+  }
+}
+close $fh;
---
+++ scripts/elasticsearch/load_index_files.sh
@@ -0,0 +1,257 @@
+#!/bin/bash
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/1000_genomes_project/1000genomes.sequence.index \
+  -data_collection 1000genomes \
+  -data_type sequence \
+  -use_column_headers \
+  -url_column FASTQ_ENA_PATH \
+  -md5_column MD5 \
+  -withdrawn_column WITHDRAWN \
+  -sample_column SAMPLE_NAME \
+  -analysis_group_column ANALYSIS_GROUP \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/1000_genomes_project/1000genomes.high_coverage.GRCh38DH.alignment.index \
+  -data_collection 1000genomes \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column CRAM \
+  -md5_column CRAM_MD5 \
+  -url_column CRAI \
+  -md5_column CRAI_MD5 \
+  -url_column BAS \
+  -md5_column BAS_MD5 \
+  -infer_sample \
+  -analysis_group high_coverage \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/1000_genomes_project/1000genomes.low_coverage.GRCh38DH.alignment.index \
+  -data_collection 1000genomes \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column CRAM \
+  -md5_column CRAM_MD5 \
+  -url_column CRAI \
+  -md5_column CRAI_MD5 \
+  -url_column BAS \
+  -md5_column BAS_MD5 \
+  -infer_sample \
+  -analysis_group low_coverage \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/1000_genomes_project/1000genomes.exome.GRCh38DH.alignment.index \
+  -data_collection 1000genomes \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column CRAM \
+  -md5_column CRAM_MD5 \
+  -url_column CRAI \
+  -md5_column CRAI_MD5 \
+  -url_column BAS \
+  -md5_column BAS_MD5 \
+  -infer_sample \
+  -analysis_group exome \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/hgsv_sv_discovery/illumina_rna.sequence.index \
+  -data_collection hgsv_sv_discovery \
+  -data_type sequence \
+  -use_column_headers \
+  -url_column ENA_FILE_PATH \
+  -md5_column MD5SUM \
+  -sample_column SAMPLE_NAME \
+  -analysis_group_column ANALYSIS_GROUP \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/hgsv_sv_discovery/illumina_wgs.GRCh38.alignment.index \
+  -data_collection hgsv_sv_discovery \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column CRAM \
+  -md5_column CRAM_MD5 \
+  -url_column CRAI \
+  -md5_column CRAI_MD5 \
+  -url_column BAS \
+  -md5_column BAS_MD5 \
+  -infer_sample \
+  -analysis_group high_coverage \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/hgsv_sv_discovery/illumina_wgs.sequence.index \
+  -data_collection hgsv_sv_discovery \
+  -data_type sequence \
+  -use_column_headers \
+  -url_column ENA_FILE_PATH \
+  -md5_column MD5SUM \
+  -sample_column SAMPLE_NAME \
+  -analysis_group_column ANALYSIS_GROUP \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/hgsv_sv_discovery/illumina_wgs.sequence.index \
+  -data_collection hgsv_sv_discovery \
+  -data_type sequence \
+  -use_column_headers \
+  -url_column ENA_FILE_PATH \
+  -md5_column MD5SUM \
+  -sample_column SAMPLE_NAME \
+  -analysis_group_column ANALYSIS_GROUP \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/illumina_platinum_pedigree/illumina_platinum_ped.sequence.index \
+  -data_collection illumina_platinum_ped \
+  -data_type sequence \
+  -use_column_headers \
+  -url_column FASTQ_FILE \
+  -md5_column MD5 \
+  -sample_column SAMPLE_NAME \
+  -withdrawn_column WITHDRAWN \
+  -analysis_group_column ANALYSIS_GROUP \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/data_collections/illumina_platinum_pedigree/illumina_platinum_ped.GRCh38DH.alignment.index \
+  -data_collection illumina_platinum_ped \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column CRAM \
+  -md5_column CRAM_MD5 \
+  -url_column CRAI \
+  -md5_column CRAI_MD5 \
+  -url_column BAS \
+  -md5_column BAS_MD5 \
+  -infer_sample \
+  -analysis_group illumina_platinum_ped \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/phase3/20130502.phase3.sequence.index \
+  -data_collection phase3 \
+  -data_type sequence \
+  -use_column_headers \
+  -url_column FASTQ_FILE \
+  -md5_column MD5 \
+  -sample_column SAMPLE_NAME \
+  -withdrawn_column WITHDRAWN \
+  -analysis_group_column ANALYSIS_GROUP \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/phase3/20130502.phase3.exome.alignment.index \
+  -data_collection phase3 \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column "BAM FILE" \
+  -md5_column "BAM MD5" \
+  -url_column "BAI FILE" \
+  -url_column "BAI FILE" \
+  -md5_column "BAS MD5" \
+  -md5_column "BAS MD5" \
+  -infer_sample \
+  -analysis_group exome \
+  -root ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/ \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file  /nfs/1000g-archive/vol1/ftp/phase3/20130502.phase3.low_coverage.alignment.index \
+  -data_collection phase3 \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column "BAM FILE" \
+  -md5_column "BAM MD5" \
+  -url_column "BAI FILE" \
+  -url_column "BAI FILE" \
+  -md5_column "BAS MD5" \
+  -md5_column "BAS MD5" \
+  -infer_sample \
+  -analysis_group exome \
+  -root ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/ \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/phase3/20130725.phase3.cg_sra.index \
+  -data_collection phase3 \
+  -data_type sequence \
+  -use_column_headers \
+  -url_column "SRA Relative Path" \
+  -sample_column Sample \
+  -analysis_group cg \
+  -root ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/ \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/phase3/20130820.phase3.cg_data_index \
+  -data_collection phase3 \
+  -url_column 0 \
+  -md5_column 1 \
+  -analysis_group cg \
+  -infer_sample \
+  -root ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/ \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/phase1/phase1.alignment.index \
+  -data_collection phase1 \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column "BAM FILE" \
+  -md5_column "BAM MD5" \
+  -url_column "BAI FILE" \
+  -url_column "BAI FILE" \
+  -md5_column "BAS MD5" \
+  -md5_column "BAS MD5" \
+  -infer_sample \
+  -analysis_group low_coverage \
+  -root ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/ \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file   /nfs/1000g-archive/vol1/ftp/phase1/phase1.exome.alignment.index \
+  -data_collection phase1 \
+  -data_type alignment \
+  -use_column_headers \
+  -url_column "BAM FILE" \
+  -md5_column "BAM MD5" \
+  -url_column "BAI FILE" \
+  -url_column "BAI FILE" \
+  -md5_column "BAS MD5" \
+  -md5_column "BAS MD5" \
+  -infer_sample \
+  -analysis_group exome \
+  -root ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/ \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/pilot_data/pilot_data.sequence.index \
+  -data_collection pilot \
+  -data_type sequence \
+  -url_column 0 \
+  -md5_column 1 \
+  -sample_column 9 \
+  -withdrawn_column 20 \
+  -analysis_group_column pilot_data \
+  -root ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/pilot_data/ \
+  -dbpass $RESEQTRACK_PASS
+
+perl load_index_file.mysql.pl \
+  -file /nfs/1000g-archive/vol1/ftp/pilot_data/pilot_data.alignment.index \
+  -data_collection pilot \
+  -data_type alignment \
+  -url_column 0 \
+  -md5_column 1 \
+  -url_column 4 \
+  -md5_column 5 \
+  -url_column 6 \
+  -md5_column 7 \
+  -sample_column 3 \
+  -analysis_group_column pilot_data \
+  -root ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/pilot_data/ \
+  -dbpass $RESEQTRACK_PASS
