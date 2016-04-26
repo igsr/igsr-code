331c6dee39be3513b36e4b10b8e83723bf6e0915
streeter <streeter@ebi.ac.uk>
2016-04-26 14:52:56 +0100

    Script for nightly update process

---
+++ shell/update_elasticsearch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ES_SCRIPTS=`dirname $0`/../scripts/elasticsearch
+
+perl $ES_SCRIPTS/load_current_tree.mysql.pl \
+  -dbpass $RESEQTRACK_PASS \
+  -ftp -check_timestamp
+
+perl $ES_SCRIPTS/load_files.es.pl \
+  -dbpass $RESEQTRACK_PASS \
+  -check_timestamp \
+  -es_host ves-hx-e4:9200 \
+  -es_index_name igsr_beta
+
+perl $ES_SCRIPTS/sync_hx_hh.es.pl \
+  -dbpass $RESEQTRACK_PASS \
+  -check_timestamp \
+  -from_es_host ves-hx-e4:9200 \
+  -to ves-pg-e4:9200 \
+  -to ves-oy-e4:9200 \
+  -repo hx_hh_sync \
+  -es_index_name igsr_beta \
+  -project_name reseq-info
