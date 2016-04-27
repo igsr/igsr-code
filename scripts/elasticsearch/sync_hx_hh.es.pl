51afa67b67cf349c967685e7cfa97d826dcf0b16
istreeter <streeter@ebi.ac.uk>
2016-04-27 15:59:33 +0100

    sync script to use aliases again, because I cannot restore indices when they are open

--- scripts/elasticsearch/sync_hx_hh.es.pl
+++ scripts/elasticsearch/sync_hx_hh.es.pl
@@ -35,7 +35,8 @@ my @es_to = map {Search::Elasticsearch->new(nodes => "$_:9200", client => '1_0::
 my $es_from = Search::Elasticsearch->new(nodes => "$from_es_host:9200", client => '1_0::Direct');
 
 my ($sec,$min,$hour,$day,$month,$year) = localtime();
-my $snapshot_name = sprintf("%s_%04d%02d%02d_%02d%02d%02d", $snapshot_prefix, $year+1900, $month+1, $day, $hour, $min, $sec);
+my $snapshot_suffix = sprintf("_%04d%02d%02d_%02d%02d%02d", $year+1900, $month+1, $day, $hour, $min, $sec);
+my $snapshot_name = $snapshot_prefix.$snapshot_suffix;
 
 my $repo_res = $es_from->snapshot->get_repository(
     repository => $repo,
@@ -63,21 +64,54 @@ foreach my $host (@to_es_host) {
       or die join("\n", "error syncing $repo_dir to $host", $rsync->err);
 }
 
-exit if ! scalar @restore_indices;
-
 foreach my $es (@es_to) {
+  foreach my $restore_index (@restore_indices) {
 
-  eval{$es->snapshot->restore(
-    repository => $repo,
-    snapshot => $snapshot_name,
-    wait_for_completion => 1,
-    body => {
-        indices => join(',', @restore_indices),
-        include_global_state => 0,
+    my $get_alias_res = eval{return $es->indices->get_alias(
+      index => $restore_index,
+    );};
+    if (my $error = $@) {
+      die "error getting index alias for $restore_index: ".$error->{text};
     }
-  );};
-  if (my $error = $@) {
-    die "error restoring snapshot $snapshot_name from $repo: ".$error->{text};
+    my @existing_aliases = grep {$_->{aliases}{$restore_index}} keys %$get_alias_res;
+    die "unexpected number of existing aliases @existing_aliases" if scalar @existing_aliases != 1;
+    my $old_index_name = $existing_aliases[0];
+
+    my $new_index_name = $restore_index.$snapshot_suffix;
+    eval{$es->snapshot->restore(
+      repository => $repo,
+      snapshot => $snapshot_name,
+      wait_for_completion => 1,
+      body => {
+          indices => join(',', @restore_indices),
+          include_global_state => 0,
+          rename_pattern => $restore_index,
+          rename_replacement => $new_index_name,
+      }
+    );};
+    if (my $error = $@) {
+      die "error restoring snapshot $snapshot_name from $repo for $restore_index: ".$error->{text};
+    }
+
+    eval{$es->indices->update_aliases(
+      body => {
+        actions => [
+          {add => {alias => $restore_index, index => $new_index_name}},
+          {remove => {alias => $restore_index, index => $old_index_name}},
+        ]
+      }
+    );};
+    if (my $error = $@) {
+      die "error changing alias from $old_index_name to $new_index_name for index $restore_index: ".$error->{text};
+    }
+
+    eval{$es->indices->delete(
+      index => $old_index_name,
+    );};
+    if (my $error = $@) {
+      die "error deleting old index $old_index_name: ".$error->{text};
+    }
+
   }
 
 }
